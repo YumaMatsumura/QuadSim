1b646214d6f3484f77525519bc8127b0d398e273
YumaMatsumura-kufusha <y.matsumura@kufusha.com>
2023-09-29 15:11:01 +0900

    First commit

---
+++ models/quad_description_launch/launch/display.launch.py
@@ -0,0 +1,105 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import GroupAction
+from launch.conditions import IfCondition
+from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PythonExpression
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def get_description_pkg_name(robot_name):
+    return PythonExpression(['"', robot_name, '_description', '"'])
+
+
+def get_xacro_file_name(robot_name):
+    return PythonExpression(['"', robot_name, '.urdf.xacro', '"'])
+
+
+def get_xacro_path(robot_name):
+    return PathJoinSubstitution(
+        [
+            FindPackageShare(get_description_pkg_name(robot_name)),
+            'urdf',
+            get_xacro_file_name(robot_name),
+        ]
+    )
+
+
+def generate_launch_description():
+    # Create the launch configuration variables
+    robot = LaunchConfiguration('robot')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ignition = LaunchConfiguration('use_ignition')
+    use_rviz = LaunchConfiguration('use_rviz')
+    declare_robot_cmd = DeclareLaunchArgument(
+        'robot', default_value='simple_quad', description='Robot name to start.'
+    )
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time', default_value='True', description='Use simulation (Gazebo) clock if true'
+    )
+    declare_use_ignition_cmd = DeclareLaunchArgument(
+        'use_ignition',
+        default_value='True',
+        description='Use ignition gazebo if true, use gazebo if false',
+    )
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        'use_rviz', default_value='True', description='Whether to use rviz'
+    )
+
+    # Get the directory
+    rviz_file = PathJoinSubstitution(
+        [FindPackageShare('quad_description_launch'), 'rviz', 'display.rviz']
+    )
+    xacro_file = get_xacro_path(robot)
+
+    robot_description = Command(['xacro', ' ', xacro_file])
+
+    # Create nodes
+    bringup_display_nodes = GroupAction(
+        [
+            Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
+                output='screen',
+                parameters=[
+                    {'use_sim_time': use_sim_time},
+                    {'robot_description': robot_description},
+                ],
+            ),
+            Node(
+                package='joint_state_publisher',
+                executable='joint_state_publisher',
+                name='joint_state_publisher',
+                output='screen',
+                parameters=[{'use_sim_time': use_sim_time}],
+            ),
+            Node(
+                condition=IfCondition(use_rviz),
+                package='joint_state_publisher_gui',
+                executable='joint_state_publisher_gui',
+                name='joint_state_publisher_gui',
+            ),
+            Node(
+                condition=IfCondition(use_rviz),
+                package='rviz2',
+                executable='rviz2',
+                output='screen',
+                arguments=['-d', rviz_file],
+                parameters=[{'use_sim_time': use_sim_time}],
+            ),
+        ]
+    )
+
+    return LaunchDescription(
+        [
+            declare_robot_cmd,
+            declare_use_sim_time_cmd,
+            declare_use_ignition_cmd,
+            declare_use_rviz_cmd,
+            bringup_display_nodes,
+        ]
+    )
---
+++ simulator/quad_ignition/launch/empty_world.launch.py
@@ -0,0 +1,132 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import ExecuteProcess
+from launch.actions import IncludeLaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Get the file directory
+    display_launch_path = PathJoinSubstitution(
+        [FindPackageShare('quad_description_launch'), 'launch', 'display.launch.py']
+    )
+    gz_launch_path = PathJoinSubstitution(
+        [FindPackageShare('ros_gz_sim'), 'launch', 'gz_sim.launch.py']
+    )
+    # gui_config_path = PathJoinSubstitution(
+    #    [FindPackageShare('quad_ignition'), 'gui', 'gui.config']
+    # )
+
+    # Create the launch configuration variables
+    robot = LaunchConfiguration('robot')
+    use_rviz = LaunchConfiguration('use_rviz')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    pose = {
+        'x': LaunchConfiguration('x_pose', default='0.0'),
+        'y': LaunchConfiguration('y_pose', default='0.0'),
+        'z': LaunchConfiguration('z_pose', default='0.2'),
+        'R': LaunchConfiguration('roll', default='0.0'),
+        'P': LaunchConfiguration('pitch', default='0.0'),
+        'Y': LaunchConfiguration('yaw', default='0.0'),
+    }
+    declare_robot_cmd = DeclareLaunchArgument(
+        'robot', default_value='simple_quad', description='Robot name to start.'
+    )
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        'use_rviz', default_value='True', description='Whether to use rviz'
+    )
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time', default_value='true', description='Use simulation (Gazebo) clock if true'
+    )
+
+    # Create nodes
+    def create_gz_node():
+        gz_arguments = []
+
+        def add_gz_node_arg(key: str, value: str):
+            gz_arguments.append(key)
+            gz_arguments.append(value)
+
+        add_gz_node_arg('-name', robot)
+        add_gz_node_arg('-x', pose['x'])
+        add_gz_node_arg('-y', pose['y'])
+        add_gz_node_arg('-z', pose['z'])
+        add_gz_node_arg('-R', pose['R'])
+        add_gz_node_arg('-P', pose['P'])
+        add_gz_node_arg('-Y', pose['Y'])
+        add_gz_node_arg('-topic', '/robot_description')
+        add_gz_node_arg('-allow_renaming', 'true')
+        node = Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=gz_arguments,
+            output='screen',
+        )
+        return node
+
+    def load_controller(controller_name: str):
+        def set_to_active_cmd():
+            _cmd = ['ros2 control load_controller --set-state active']
+            _cmd.append(controller_name)
+            return _cmd
+
+        process = ExecuteProcess(
+            cmd=set_to_active_cmd(),
+            shell=True,
+            output='screen',
+        )
+        return process
+
+    spawn_entity = create_gz_node()
+    gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{'use_sim_time': True}],
+        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+    )
+    load_joint_state_broadcaster = load_controller('joint_state_broadcaster')
+    load_position_controller = load_controller('joint_group_position_controller')
+
+    return LaunchDescription(
+        [
+            declare_robot_cmd,
+            declare_use_rviz_cmd,
+            declare_use_sim_time_cmd,
+            # ===== display launch (RViz2) ===== #
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(display_launch_path),
+                launch_arguments={
+                    'robot': robot,
+                    'use_rviz': use_rviz,
+                    'use_ignition': 'true',
+                    'use_sim_time': use_sim_time,
+                }.items(),
+            ),
+            # ===== Gazebo ===== #
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(gz_launch_path),
+                launch_arguments=[('gz_args', [' -r -v 3 empty.sdf'])],
+            ),
+            spawn_entity,
+            gz_bridge,
+            # ===== Ros2 Control ===== #
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=spawn_entity, on_exit=[load_joint_state_broadcaster]
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=load_joint_state_broadcaster, on_exit=[load_position_controller]
+                )
+            ),
+        ]
+    )
